960775339d9f0cc1b27a0b39f4e9c9373a1f16f0
paulnitu <nitupaul91@gmail.com>
2024-01-13 21:17:24 +0200

    add dummy emoji

--- Sources/KeyboardKit/Emojis/Emoji+KeyboardWrapper.swift
+++ Sources/KeyboardKit/Emojis/Emoji+KeyboardWrapper.swift
@@ -17,17 +17,57 @@ public extension Emoji {
      */
     struct KeyboardWrapper: View {
         
+        let actionHandler: KeyboardActionHandler
+        let keyboardContext: KeyboardContext
+        let styleProvider: KeyboardStyleProvider
+        
         init(
             actionHandler: KeyboardActionHandler,
             keyboardContext: KeyboardContext,
             calloutContext: CalloutContext?,
             styleProvider: KeyboardStyleProvider
-        ) {}
+        ) {
+            self.actionHandler = actionHandler
+            self.keyboardContext = keyboardContext
+            self.styleProvider = styleProvider
+        }
         
         public var body: some View {
-            EmptyView()
+            EmojiKeyboardView(
+                actionHandler: actionHandler,
+                keyboardContext: keyboardContext,
+                style: KeyboardStyle.EmojiKeyboard.standard(for: keyboardContext)
+            )
         }
         
         static let isPro = false
+    }
+}
+
+struct EmojiKeyboardView: View {
+    
+    var actionHandler: KeyboardActionHandler
+    var keyboardContext: KeyboardContext
+    let style: KeyboardStyle.EmojiKeyboard
+    
+    let emojis: [String] = ["😀", "😃", "😄", "😁"]
+    
+    var body: some View {
+        let rows = Array(repeating: GridItem(.fixed(style.itemSize + style.verticalPadding * 2), spacing: style.verticalItemSpacing), count: style.rows)
+        
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: rows, spacing: style.horizontalItemSpacing) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Button(action: {
+                        actionHandler.handle(.emoji(emoji))
+                    }) {
+                        Text(emoji)
+                            .font(style.itemFont)
+                            .frame(width: style.itemSize, height: style.itemSize)
+                            .padding(.vertical, style.verticalPadding)
+                    }
+                }
+            }
+        }
     }
 }
